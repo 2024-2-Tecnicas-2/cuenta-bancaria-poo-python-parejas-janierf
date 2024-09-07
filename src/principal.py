e524cdec2d445a6956684e07ce486dfdc54fe841
Your Name <you@example.com>
2024-09-07 10:02:38 -0500

    Realizo la clase principal y agrego mis dos pruebas unitarias comprobadas

--- src/principal.py
+++ src/principal.py
@@ -1,2 +1,28 @@
+import cuenta_bancaria
+
+
 if __name__ == '__main__':
-    # TODO: Adiciona aquí el código que deseas para la Cuenta Bancaria.
+   def main():
+    cuenta = cuenta_bancaria("Juan Perez", "123456789", 1000)
+
+   
+    print(f"Titular de la cuenta: {cuenta.getTitular()}")
+    print(f"Número de cuenta: {cuenta.getNumeroCuenta()}")
+    print(f"Saldo inicial: {cuenta.getSaldo()}")
+
+    cuenta.ingresar(500)
+    print(f"Saldo después de ingresar: {cuenta.getSaldo()}")
+
+    
+    cuenta.retirar(300)
+    print(f"Saldo después de retirar: {cuenta.getSaldo()}")
+
+   
+    print(f"Saldo con interés: {cuenta.calcularInteres()}")
+
+   
+    cuenta.setTipoInteres(3)
+    print(f"Nuevo saldo con interés: {cuenta.calcularInteres()}")
+
+if __name__ == "__main__":
+    main()
--- tests/test_cuenta_bancaria.py
+++ tests/test_cuenta_bancaria.py
@@ -1,15 +1,21 @@
-import sys
-import os
 import unittest
 
-# Add the 'src' directory to the Python path so 'calculadora' can be imported
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+import cuenta_bancaria
 
 
-class TestCalcular(unittest.TestCase):
-    # TODO Adiciona tus pruebas unitarias aquí.
-    # Ejemplo:   
-    # def test_suma(self):
-    #     valor_esperado = 3
-    #     valor_actual = calcular(1, 2, '+')
-    #     self.assertEqual(valor_esperado, valor_actual)
+class TestCuentaBancaria(unittest.TestCase):
+    
+    def setUp(self):
+      
+        self.cuenta = cuenta_bancaria("Juan Perez", "123456789", 1000)
+
+  
+    def test_getTitular(self):
+        valor_esperado = "Juan Perez"
+        valor_actual = self.cuenta.getTitular()
+        self.assertEqual(valor_esperado, valor_actual)
+
+    def test_getSaldo_inicial(self):
+        valor_esperado = 1000
+        valor_actual = self.cuenta.getSaldo()
+        self.assertEqual(valor_esperado, valor_actual)
